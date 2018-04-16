784e5861d5cb1b2c954fe9da3439f4adae9e35e1
Stan Hughes <sah05@aquitysso.com>
2018-04-16 17:36:01 -0400

    Separate device 1

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -2459,7 +2459,7 @@ module.exports.blsPMDSite = {
 module.exports.blsPMDSiteLiveMap1 = {
       "deviceID": "TW9ja3Rlc3REZXZpY2UwMXwxNTIzOTEyNzAwMzI0",
       "longitude": -84.0370122234284,
-      "latitude":   33.6835395009525,
+      "latitude":   33.6835395006525,
       "timeStamp": "2018-04-16T15:00:43.794Z",
       "floorID": 22,
       "floorSpaceID": 30,
