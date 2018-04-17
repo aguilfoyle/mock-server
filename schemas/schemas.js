ea277f39b3e4f279e4b3addec4a13075c45963ae
Stan Hughes <sah05@aquitysso.com>
2018-04-17 16:05:04 -0400

    position known devices

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -2471,7 +2471,7 @@ module.exports.blsPMDSiteLiveMap1 = {
 module.exports.blsPMDSiteLiveMap2 = {
        "deviceID": "TW9ja3Rlc3REZXZpY2UwMnwxNTIzOTA0MDg3ODk5",
        "longitude": -84.0370122223284,
-       "latitude": 33.6835095007525,
+       "latitude": 33.6835195007525,
        "timeStamp": "2018-04-16T15:00:45.627Z",
        "floorID": 22,
        "floorSpaceID": 30,
