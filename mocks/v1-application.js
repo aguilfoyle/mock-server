3bb53d27a7f119f2a19e55242c447a6ca5b694b1
DLD05 <danielle.dampier@acuitybrands.com>
2020-09-22 13:42:42 -0400

    Changing

--- mocks/v1-application.js
+++ mocks/v1-application.js
@@ -93,7 +93,7 @@ var pizza = {
     name: 'pizza',
     mockRoute: '\/api\/v1\/order\/.*', //a regex for the route, ex. '\/api\/steps\/.*\/users' is a valid route
     testScope: 'success', //success=200 & a scenario response | notFound=404 | error=500 and there's many more...
-    testScenario: 'noProducingSomeAlerts', //change this to one of hte scenario names below and restart the mock server to get new data
+    testScenario: 'noAwaitingShipmentSomeAlerts', //change this to one of hte scenario names below and restart the mock server to get new data
     latency: '500-3000', //add this line ot implement 1-5 seconds of random latency per call
     jsonTemplate: [{
         //you can use regular javascript to create objects to be served
@@ -53468,6 +53468,37 @@ var pizza = {
                     result[i].bucketType = "Shipping";
                 }
                 if(i >= 20){
+                    result[i].bucketType = "Delivery";
+                }
+            }
+            return JSON.stringify({
+                result: result
+            });
+        },
+        noAwaitingShipmentSomeAlerts: function () {
+            var result = [];
+            var count = 30;
+            for (var i = 0; i < count; i++) {
+                result.push(JSON.parse(JSON.stringify(schemas.line)));
+                result[i].LineNum = i;
+                if(i >= 0 && i <=4){
+                    result[i].bucketType = "Order Processing";
+                }
+                if(i >= 5 && i <=9){
+                    result[i].bucketType = "Scheduled";
+                }
+                if(i >= 10 && i <=14){
+                    result[i].bucketType = "Scheduled";
+                    result[i].alerts = [];
+                }
+                if(i >= 15 && i <=19){
+                    result[i].bucketType = "Producing";
+                }
+                if(i >= 20 && i <=24){
+                    result[i].bucketType = "Producing";
+                    result[i].alerts = [];
+                }
+                if(i >= 25){
                     result[i].bucketType = "Delivery";
                 }
             }
