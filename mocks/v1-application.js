ae4dd7b08039184827ea6cd1da589329e0983653
DLD05 <danielle.dampier@acuitybrands.com>
2020-09-22 14:24:24 -0400

    Changing

--- mocks/v1-application.js
+++ mocks/v1-application.js
@@ -93,7 +93,7 @@ var pizza = {
     name: 'pizza',
     mockRoute: '\/api\/v1\/order\/.*', //a regex for the route, ex. '\/api\/steps\/.*\/users' is a valid route
     testScope: 'success', //success=200 & a scenario response | notFound=404 | error=500 and there's many more...
-    testScenario: 'allShipped', //change this to one of hte scenario names below and restart the mock server to get new data
+    testScenario: 'allBucketsSomeAlerts', //change this to one of hte scenario names below and restart the mock server to get new data
     latency: '500-3000', //add this line ot implement 1-5 seconds of random latency per call
     jsonTemplate: [{
         //you can use regular javascript to create objects to be served
@@ -53782,7 +53782,7 @@ var pizza = {
                 result: result
             });
         },
-        allBucketsWithAlerts: function () {
+        allBucketsSomeAlerts: function () {
             var result = [];
             var count = 35;
             for (var i = 0; i < count; i++) {
@@ -53816,7 +53816,7 @@ var pizza = {
                 result: result
             });
         },
-        allBucketsWithoutAlerts: function () {
+        allBucketsNoAlerts: function () {
             var result = [];
             var count = 25;
             for (var i = 0; i < count; i++) {
