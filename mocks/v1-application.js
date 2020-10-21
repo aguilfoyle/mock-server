0a315a05e185c70f4416ecb94c73f4e900bd07b6
DLD05 <danielle.dampier@acuitybrands.com>
2020-10-21 10:40:15 -0400

    Changing

--- mocks/v1-application.js
+++ mocks/v1-application.js
@@ -55923,18 +55923,18 @@ var pizza = {
                         {
                             "alertType": "2005"
                         },
-                        // {
-                        //     "alertType": "3001"
-                        // },
+                        {
+                            "alertType": "3001"
+                        },
                         // {
                         //     "alertType": "3002.1"
                         // },
                         // {
                         //     "alertType": "3002.2"
                         // },
-                        {
-                            "alertType": "4001.1"
-                        },
+                        // {
+                        //     "alertType": "4001.1"
+                        // },
                         // {
                         //     "alertType": "4001.2"
                         // },
