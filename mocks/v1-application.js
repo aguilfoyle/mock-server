e6e38e3848c2c806b25870cc322bac73b6816445
DLD05 <danielle.dampier@acuitybrands.com>
2020-10-22 11:56:00 -0400

    Changing

--- mocks/v1-application.js
+++ mocks/v1-application.js
@@ -56185,30 +56185,30 @@ var pizza = {
                         {
                             "alertType": "3001"
                         },
-                        {
-                            "alertType": "3002.1"
-                        },
-                        {
-                            "alertType": "3002.2"
-                        },
-                        {
-                            "alertType": "4001.1"
-                        },
-                        {
-                            "alertType": "4001.2"
-                        },
-                        {
-                            "alertType": "4001.3"
-                        },
-                        {
-                            "alertType": "4001.4"
-                        },
-                        {
-                            "alertType": "4001.5"
-                        },
-                        {
-                            "alertType": "4001.6"
-                        },
+                        // {
+                        //     "alertType": "3002.1"
+                        // },
+                        // {
+                        //     "alertType": "3002.2"
+                        // },
+                        // {
+                        //     "alertType": "4001.1"
+                        // },
+                        // {
+                        //     "alertType": "4001.2"
+                        // },
+                        // {
+                        //     "alertType": "4001.3"
+                        // },
+                        // {
+                        //     "alertType": "4001.4"
+                        // },
+                        // {
+                        //     "alertType": "4001.5"
+                        // },
+                        // {
+                        //     "alertType": "4001.6"
+                        // },
                         {
                             "alertType": "6001"
                         }
